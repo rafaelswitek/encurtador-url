4edab70bec99ccad323ed771a6ddfc766e675c13
Rafael Gonçalves <rafaelswitek@gmail.com>
2021-08-05 12:00:26 -0300

    Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{path}', [UrlController::class, 'redirect']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
